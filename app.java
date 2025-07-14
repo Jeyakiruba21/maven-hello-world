858c44db665cc8bd7619afe3e85b6b16c24431b6
jeyakiruba21 <jeyakirubakaran21@gmail.com>
2025-07-14 20:25:09 +0530

    Updated greeting message for CI test

--- app.java
+++ app.java
@@ -2,6 +2,6 @@ package com.devopslab4;
 
 public class App {
     public static void main(String[] args) {
-        System.out.println("Hello, Maven World!");
+        System.out.println("Hello, Jenkins Continuous Integration!");
     }
 }
